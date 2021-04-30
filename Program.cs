9559925d8b73af0110c2d036ed65439dbd876bb6
n1klas2305 <83063341+n1klas2305@users.noreply.github.com>
2021-04-30 11:05:37 +0200

    Weiterführung randomShipPlace

--- Program.cs
+++ Program.cs
@@ -8,5 +8,47 @@ namespace Programmierung
         {
             Console.WriteLine("Hello World!");
         }
+
+        public static void randomShipPlace()
+        {
+            int shipCount = 1;
+            string[,] playground = new string[20, 20];
+            for (int i = 5; i <= 2; i--)
+            {
+                for (int r = 0; r < shipCount; r++)
+                {
+                    int randomX = new Random().Next(0, 20);
+                    int randomY = new Random().Next(0, 20);
+                    playground[randomX, randomY] = "x";
+                    int randomDirection = new Random().Next(0, 4);
+                    if(randomDirection == 0) {
+                        int help = i;
+                        while(help > 0) {
+                            playground[randomX, randomY++] = "x";
+                            help--;
+                        }
+                    } else if(randomDirection == 1) {
+                        int help = i;
+                        while(help > 0) {
+                            playground[randomX, randomY--] = "x";
+                            help--;
+                        }
+                    } else if(randomDirection == 2) {
+                        int help = i;
+                        while(help > 0) {
+                            playground[randomX, randomY--] = "x";
+                            help--;
+                        }
+                    } else if(randomDirection == 3) {
+                        int help = i;
+                        while(help > 0) {
+                            playground[randomX, randomY--] = "x";
+                            help--;
+                        }
+                    }
+                }
+                shipCount++;
+            }
+        }
     }
 }
